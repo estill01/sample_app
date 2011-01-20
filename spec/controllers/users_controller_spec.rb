8bc262a1823f0c67d615fb48438060755a81438d
estill01 <ehs.stillman@gmail.com>
2011-01-19 20:20:43 -0500

    Ch8 Ex1

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -13,6 +13,27 @@ describe UsersController do
       get :new
       response.should have_selector(:title, :content => "Sign up")
     end    
+    
+    it "should have a name field" do
+      get :new
+      response.should have_selector("input[name='user[name]'][type='text']")
+    end
+    
+    it "should have an email field" do
+      get :new
+      response.should have_selector("input[name='user[email]'][type='text']")
+    end
+    
+    it "should have a password field" do
+      get :new
+      response.should have_selector("input[name='user[password]'][type='password']")
+    end
+    
+    it "should have a password confirmation field" do
+      get :new
+      response.should have_selector("input[name='user[password_confirmation]'][type='password']")
+    end
+    
   end
 
   describe "GET 'show'" do
