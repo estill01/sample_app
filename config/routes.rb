4df7613c3091cad9ad085b2929704d586b870198
estill01 <ehs.stillman@gmail.com>
2011-01-21 14:32:22 -0500

    initial user profile edit functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 SampleApp::Application.routes.draw do
   resources :users
-  resources :sessions, :only => [:new, :create, :destroy], 
-                       :constraints => { :protocol => "https" }
+  resources :sessions, :only => [:new, :create, :destroy]
 
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
